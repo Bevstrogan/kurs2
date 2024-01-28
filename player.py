b01b2a6660ecccd363069ed90f5d372c302999ee
Bevstrogan <148994331+Bevstrogan@users.noreply.github.com>
2024-01-28 09:29:36 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,13 +1,18 @@
+"""Класс направленный на введенные пользователем слова"""
 class Player():
     def __init__(self, name: str, used_subwords: list):
         self.name = name
         self.used_subwords: list = used_subwords
 
+    """Функция считает кол-во введенных пользователем
+    правильных слов"""
     def count_words(self):
         return len(self.used_subwords)
 
+    """Функция добавляем слово к списку использованных слов"""
     def add_word(self, word):
         self.used_subwords.append(word)
 
+    """функция проверяет былоли использованно раннее введенное пользователем слово"""
     def has_word(self, word):
         return word in self.used_subwords
