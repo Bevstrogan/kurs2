8640fb6cc293591bb21576fd5efe630b77760970
Bevstrogan <148994331+Bevstrogan@users.noreply.github.com>
2024-01-28 09:23:51 +0300

    Update basic_words.py
    Описание

--- basic_words.py
+++ basic_words.py
@@ -1,10 +1,14 @@
+"""Функция направленна на проверку введенного игроком слова"""
 class BasicWord():
     def __init__(self, word: str, subwords: list):
         self.word = word
         self.subwords = subwords
 
+
+"""Функция выводит кол-во слов, которых можно собрать из исходного слова"""
     def count_subwords(self):
         return len(self.subwords)
-
+        
+"""Функция проверяет есть ли введенное слово в списк ответов"""
     def has_word(self, player_word):
         return player_word in self.subwords
