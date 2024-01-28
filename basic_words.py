4e0ff515781ae325ba6daaabd01464a6876ab89f
дмитрий алехин <bevstrogan@gmail.com>
2024-01-28 09:41:14 +0300

    исправление

--- basic_words.py
+++ basic_words.py
@@ -5,10 +5,10 @@ class BasicWord():
         self.subwords = subwords
 
 
-"""Функция выводит кол-во слов, которых можно собрать из исходного слова"""
+    """Функция выводит кол-во слов, которых можно собрать из исходного слова"""
     def count_subwords(self):
         return len(self.subwords)
         
-"""Функция проверяет есть ли введенное слово в списк ответов"""
+    """Функция проверяет есть ли введенное слово в списк ответов"""
     def has_word(self, player_word):
         return player_word in self.subwords
